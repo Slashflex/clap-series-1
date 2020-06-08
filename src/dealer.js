12a7d4bec6c84e3cfd17d6485ea643653e09354c
slashflex <pro.davidsaoud@gmail.com>
2020-06-08 13:41:32 +0200

    add: unit test for dealer.js (passing)

--- src/dealer.js
+++ src/dealer.js
@@ -2,16 +2,29 @@
  * /!\ À faire seul !
  * Le but de cette fonction est de générer deux tableaux contenants 5 cartes différentes
  * il ne doit y avoir aucun doublon dans les tableaux !
- * 
- * Exemple dealer() => [["As", "3s", "2h", "8d", "8s"], ["As", "3s", "2h", "8d", "8s"]]
- * 
+ *
+ * Exemple dealer() => [['As', '3s', '2h', '8d', '8s'], ['As', '3s', '2h', '8d', '8s']]
+ *
  * Une carte est une chaîne de caractère qui contient deux parties :
  * - La valeur de la carte (ordre croissant): 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
  * - Le type de la carte (ordre croissant) : d => ♦, c => ♣, h => ♥, s => ♠
- * 
+ *
  */
 function dealer() {
-  // CODE HERE
+  const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const cardType = ["d", "c", "h", "s"];
+  const combined = [];
+
+  for (const value of cardValue) {
+    for (const type of cardType) {
+      combined.push(value + type);
+    }
+  }
+
+  combined.sort(() => Math.random() - 0.5);
+  const result = [combined.slice(0, 5), combined.slice(5, 10)]
+
+	return result
 }
 
 export { dealer };
